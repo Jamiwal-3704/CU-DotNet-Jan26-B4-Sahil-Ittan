4f57732b157c37a160559e1e6782749a7848e30b
Jamiwal-3704 <ittansahil@gmail.com>
2026-01-14 16:17:17 +0530

    commiting the 8th DAY EXERCISE AND PPTS

---
+++ DAY 8/SimpleUserLoginMessageProcessor/SimpleUserLoginMessageProcessor/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+
+namespace SimpleUserLoginMessageProcessor
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // take input mesaage from the user in particular formate
+            Console.WriteLine("Enter your message here" + "formate --> <UserName>|<LoginMessage>");
+            string message = Console.ReadLine();
+
+            // split input into userName and loginMessage
+            string[] parts = message.Split('|');
+            string userName = parts[0];
+            string loginMessage = parts[1];
+
+            // clean the message using the trim, toLower methods
+            string cleanedMessage = message.Trim().ToLower();
+            // SAHIL| LOGIN SUCCESSFULL  --> sahil| login successfull   
+
+            string standardMessage = "login successfull";
+            string status;
+
+            if(!loginMessage.Contains("successfull"))
+            {
+                status = "LOGIN FAIL";
+            }
+            else if(cleanedMessage.Equals(standardMessage))
+            {
+                status = "LOGIN SUCCESS";
+            }
+            else
+            {
+                status = "LOGIN SUCCESS (CUSTOM MESSAGE)";
+            }
+
+            // OUTPUT AFTER WHOLE PROCESS -->
+            Console.WriteLine($"user: {userName,5}");
+            Console.WriteLine($"Message: {cleanedMessage}");
+            Console.WriteLine($"Status: {status}");
+        }
+    }
+}
