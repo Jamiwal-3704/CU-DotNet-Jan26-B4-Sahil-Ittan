22f8162c4fcbd46c5c902dd81082177b45ac26d4
Jamiwal-3704 <ittansahil@gmail.com>
2026-01-21 14:55:19 +0530

    created the new folder WEEK 3 in that completed the DAY 14th EXERCISE : VowelCounter->VowelCounter

---
+++ WEEK 3/DAY 14/TextAnalyser/TextAnalyser/Program.cs
@@ -0,0 +1,48 @@
+﻿// you are developing a text anaylysis utility used in educational platform to ealuate basic string processing skills.
+
+// method contract (Mandatory)
+// int CountVowels(string input)
+
+// BASICALLY WE ONLY HAVEE TO COUNT THE VOWELS IN A STRING
+
+
+using System.Collections.Specialized;
+using System.Runtime.Serialization.Formatters;
+
+namespace TextAnalyser
+{
+    internal class Program
+    {
+        public static int CountVowels(string input)
+        {
+            int count = 0;
+            //if(input == null || input == "")
+            //{
+            //    return 0;
+            //}
+            input = input.ToLower();
+
+            foreach(char ch in input)
+            {
+                if(ch == 'a' || ch == 'e' || ch == 'i' || ch == 'o' || ch == 'u')
+                {
+                    count++;
+                }
+                else if(string.IsNullOrEmpty(input))
+                {
+                    return 0;
+                }
+            }
+
+            return count;
+        }
+
+        static void Main(string[] args)
+        {
+            Console.Write("Enter a string to analyze: ");
+            string input = Console.ReadLine();
+            int VowelCount = CountVowels(input);
+            Console.WriteLine($"number of Vowels in given string are: {VowelCount}");
+        }
+    }
+}
