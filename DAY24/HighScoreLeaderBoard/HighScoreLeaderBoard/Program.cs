cca6c9f9e2355d4a3bd9193a491ae5bb9e2df978
Jamiwal-3704 <ittansahil@gmail.com>
2026-02-03 14:52:29 +0530

    1. updated all the folder names 2. Completed the DAY24 EXERCIDE where created the LeaderBoard and perform the SortedDictionary

---
+++ DAY24/HighScoreLeaderBoard/HighScoreLeaderBoard/Program.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections.Generic;
+
+namespace HighScoreLeaderBoard
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            SortedDictionary<double, string> leaderBoard = new SortedDictionary<double, string>()
+            {
+                {55.42,"SwiftRacer"},
+                {52.10,"SpeedDemon"},
+                {58.91,"SteadyEddie"},
+                {51.05,"TurboTom"}
+            };
+
+            // printing all the leader board according to the lap timings
+            foreach (var item in leaderBoard)
+            {
+                Console.WriteLine($"{item.Value} : {item.Key}");
+            }
+
+            // find the fastest Racer in the race
+            var fastest = leaderBoard.First();
+            Console.WriteLine($"Fastest Racer: " + fastest);
+
+            leaderBoard.Remove(58.91);
+            leaderBoard.Add(54, "SteadyEddie");
+
+            Console.WriteLine("Updated LeaderBoard");
+
+            foreach(var item in leaderBoard)
+            {
+                Console.WriteLine($"Name: {item.Value} --> Speed: {item.Key}");
+            }
+        }
+    }
+}
