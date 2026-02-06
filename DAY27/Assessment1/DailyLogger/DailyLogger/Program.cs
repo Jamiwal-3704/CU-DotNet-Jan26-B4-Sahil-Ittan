88c52b731f6a0455355b793c487f00edd44d5845
Jamiwal-3704 <ittansahil@gmail.com>
2026-02-06 12:37:06 +0530

    Completed the DAY27 EXERCISE creating DailyLogger

---
+++ DAY27/Assessment1/DailyLogger/DailyLogger/Program.cs
@@ -0,0 +1,15 @@
+﻿namespace DailyLogger
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string file = @"..\..\..\journal.txt";
+            //FileStream fs = new FileStream(file, FileMode.Append, FileAccess.ReadWrite);
+            using StreamWriter fs = new StreamWriter(file, true);
+            Console.WriteLine("journal.txt is open. Please enter your journal entry:");
+            string? input = Console.ReadLine();
+            fs.WriteLine(input);
+        }
+    }
+}
